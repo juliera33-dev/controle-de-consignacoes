18b6a0b2f8465d703a5497ed6fb8301e7e4f7a15
juliera33-dev <julioteixxeiraalves08@gmail.com>
2025-08-13 22:03:03 -0300

    Fix db import and initialization across modules

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 import os
 import sys
-# DON\'T CHANGE THIS !!!
+
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from src.models.user import db
+from src.extensions import db # Importa db de extensions.py
 from src.routes.user import user_bp
 from src.routes.estoque import estoque_bp
+
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
@@ -18,7 +19,7 @@ CORS(app)
 app.register_blueprint(user_bp, url_prefix='/api')
 app.register_blueprint(estoque_bp, url_prefix='/api/estoque')
 
-# uncomment if you need to use database
+# Linhas de configuração do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or f"sqlite:///{os.path.join(os.path.dirname(__file__), 'database', 'app.db')}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -35,5 +36,4 @@ def serve(path):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
 
--- src/models/nfe.py
+++ src/models/nfe.py
@@ -1,4 +1,4 @@
-from src.models.user import db
+from src.extensions import db
 from datetime import datetime
 
 class NotaFiscal(db.Model):
